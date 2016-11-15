f61025632a8ee4a209d2ca60548ed0d8034e53da
Shin1122 <big_shin@126.com>
2016-11-15 19:45:07 +0800

    QTI-9 直播观看
    重写了自动布局。
    课程列表自适应header自动布局高度。
    
    
    Former-commit-id: d74ffcc7d725246eba1bae490e06ee7e215ddbcd

---
+++ Qatime_Student/NewViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewViewController.h
+//  Qatime_Student
+//
+//  Created by Shin on 2016/11/16.
+//  Copyright © 2016年 WWTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewViewController : UIViewController
+
+@end
